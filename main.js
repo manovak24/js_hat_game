1421840d7d7293089505b328f6bd043150f9858e
emagdnimMQT <mark.w.novak@gmail.com>
2021-04-12 16:39:31 -0400

    working on class methods for game.

--- main.js
+++ main.js
@@ -4,3 +4,23 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+    constructor(fieldArray) {
+        this.fieldArray = fieldArray;
+    }
+
+
+    //Helper method to print field in grid form
+    print() {
+        this.fieldArray.forEach(element => console.log(element.join('')))
+    }
+}
+
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+
+myField.print()
